8e5dca03d0f07c3845dd18731c017a455940557b
rahul bajaj <tb1169cg12@gmail.com>
2026-05-27 07:15:50 +0530

    Manual commit: 2026-05-27 07:15:49

--- Chapter_06_double_triple_equall/41_ternary_operator.js
+++ Chapter_06_double_triple_equall/41_ternary_operator.js
@@ -26,7 +26,7 @@ let sla=1000;
 let sla_status = response_time <=sla ? "within SLA" : "SLA breached";
 console.log(`response : ${response_time} ms-${sla_status}`);  // Output: within SLA
 
-
+//nested ternary operator
 
 let status_code = 404;
 let category = status_code <300 ? "success"
@@ -40,8 +40,28 @@ let age =26;
 let is_pramod_goa = age >=18 ? "yes" : "no";
 console.log(is_pramod_goa);  // Output: yes
 
-//nested ternary operator
+//nested ternary operator in ternary
 
 let age_pramod =10 ;
 let will_pramod_drink = age_pramod >=18 ? (age_pramod >=18 ? "will drink" :"will not drink") : "goa false";
 console.log(will_pramod_drink);  // Output: will not drink
+
+
+//greates of 2 numbers
+let number1 =20;
+let number2 =20;
+let greater_number = number1 > number2 ? "number1 is greater" 
+                    :number2 >number1 ?"number 2 is grater"
+                    : "both numbers are equal";
+console.log(greater_number);  // Output: both numbers are equal
+
+
+//greates of 3 numbers
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+
+let greatest_number_new = (num1>num2 && num1>num3)? " number1 is greater":
+                          (num2>num3 && num2>num1) ? "number 2 is greater" :
+                          (num3>num1 && num3>num2) ?"number3 is greater " :"all are equal"
+console.log(greatest_number_new);  // Output: number 2 is greater
